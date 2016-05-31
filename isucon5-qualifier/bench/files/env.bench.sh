aec5a968c162744ba9cbc85ae24d1b373d7a39d3
SatoShun <shun.sato1@gmail.com>
2016-05-31 23:27:50 +0000

    update certificate

--- isucon5-qualifier/bench/files/env.bench.sh
+++ isucon5-qualifier/bench/files/env.bench.sh
@@ -1,5 +1,5 @@
 PATH=/home/isucon/.local/ruby/bin:/usr/local/bin:/usr/local/bin:/usr/bin:/bin
-LANG=ja_JP.UTF-8
+# LANG=ja_JP.UTF-8
 RACK_ENV=production
 _JAVA_OPTIONS="-Dfile.encoding=UTF8 -Duser.timezone=JST"
 JAVA_TOOL_OPTIONS=-Dfile.encoding=UTF8
